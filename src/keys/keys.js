6755344f21b742abe15d91799a22b928a5707de8
Sam Larsen <sam@slarsen.io>
2024-09-02 03:41:26 -0400

    Remove base58 encoding and enhance mnemonic function

--- src/keys/keys.js
+++ src/keys/keys.js
@@ -1,13 +1,14 @@
 const crypto = require("crypto");
 const bip39 = require("bip39");
-const bs58 = require("bs58");
 const secp256k1 = require("secp256k1");
 const nacl = require("tweetnacl");
 
-function mnemonic() {
+function mnemonic({ words, language }) {
 	try {
-		const mnemonic = bip39.generateMnemonic();
-		return mnemonic;
+        bip39.setDefaultWordlist(language);
+        const entropy = (words / 3) * 32;
+        const mnemonic = bip39.generateMnemonic(entropy);
+		return mnemonic
 	} catch (e) {
 		return "Error: failed to generate mnemonic";
 	}
@@ -17,21 +18,19 @@ function keypair({ type }) {
 	try {
 		if (type === "ed25519") {
 			const keypair = nacl.sign.keyPair();
-			const publicKeyBase58 = bs58.encode(Buffer.from(keypair.publicKey));
-			const secretKeyBase58 = bs58.encode(Buffer.from(keypair.secretKey));
 			return {
-				publicKey: publicKeyBase58,
-				secretKey: secretKeyBase58
+				publicKey: keypair.publicKey,
+				secretKey: keypair.secretKey
 			};
 		} else if (type === "secp256k1") {
 			let privateKey;
 			do {
-				privateKey = crypto.randomBytes(32);
+				privateKey = new Uint8Array(crypto.randomBytes(32));
 			} while (!secp256k1.privateKeyVerify(privateKey));
 			const publicKey = secp256k1.publicKeyCreate(privateKey);
 			return {
-				publicKey: bs58.encode(publicKey),
-				privateKey: bs58.encode(privateKey)
+				publicKey: publicKey,
+				privateKey: privateKey
 			};
 		} else {
 			return "Error: unsupported curve type";
@@ -51,18 +50,16 @@ function keypairFromMnemonic({ mnemonic, passphrase, type }) {
 		}
 		if (type === "ed25519") {
 			const keypair = nacl.sign.keyPair.fromSeed(seed);
-			const publicKeyBase58 = bs58.encode(Buffer.from(keypair.publicKey));
-			const secretKeyBase58 = bs58.encode(Buffer.from(keypair.secretKey));
 			return {
-				publicKey: publicKeyBase58,
-				secretKey: secretKeyBase58
+				publicKey: keypair.publicKey,
+				secretKey: keypair.secretKey
 			};
 		} else if (type === "secp256k1") {
 			const privateKey = seed;
 			const publicKey = secp256k1.publicKeyCreate(privateKey);
 			return {
-				publicKey: bs58.encode(publicKey),
-				privateKey: bs58.encode(privateKey)
+				publicKey: publicKey,
+				privateKey: privateKey
 			};
 		} else {
 			return "Error: unsupported curve type";
